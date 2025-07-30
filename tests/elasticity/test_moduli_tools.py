4b504aa7f5b1001090708490e87fc4767bedb91a
donald e. boyce <donald.e.boyce@gmail.com>
2025-07-30 15:21:54 -0400

    checkpoint

--- tests/elasticity/test_moduli_tools.py
+++ tests/elasticity/test_moduli_tools.py
@@ -2,7 +2,7 @@
 import numpy as np
 import pytest
 
-from polycrystal.elasticity.moduli_tools import Cubic
+from polycrystal.elasticity.moduli_tools import Cubic, MatrixBuilder
 
 
 def check_equal(c1, c2):
@@ -24,3 +24,19 @@ class TestCubic:
         K, Gd, Gs = c.K, c.Gd, c.Gs
         cnew = Cubic.from_K_Gd_Gs(K, Gd, Gs)
         check_equal(c, cnew)
+
+
+class TestMatrixBuilder:
+    """Test MatrixBuilder class"""
+
+    def test_to_matrix(self):
+        c11, c12, c13, c22, c23, c33, c44, c55, c66 = range(1, 10)
+        m = MatrixBuilder.to_matrix(c11, c12, c13, c22, c23, c33, c44, c55, c66)
+
+        assert np.all(m == m.T)
+        assert np.all(m[:3, 3:] == 0)
+        assert np.allclose(np.diag(m), np.array([c11,c22, c33, c44, c55, c66]))
+        assert np.allclose(
+            np.array([m[0, 1], m[0, 2], m[1, 2]]),
+            np.array([c12,c13, c23])
+        )

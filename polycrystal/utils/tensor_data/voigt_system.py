982002917ca9e25c27b5d90c1f429d2926b371d8
donald e. boyce <donald.e.boyce@gmail.com>
2025-08-04 14:16:09 -0400

    added Voigt system for ttensor_data

--- polycrystal/utils/tensor_data/voigt_system.py
+++ polycrystal/utils/tensor_data/voigt_system.py
@@ -1,14 +1,14 @@
-"""Mandel System for Representing 3D Tensors"""
+"""Voigt System for Representing 3D Tensors"""
 
 import numpy as np
 
 from .base_system import BaseSystem
 
 
-class MandelSystem(BaseSystem):
-    """Mandel System for Representing 3D Tensors"""
+class VoigtSystem(BaseSystem):
+    """Voigt System for Representing 3D Tensors"""
 
-    _system_name = "Mandel"
+    _system_name = "Voigt"
 
     @classmethod
     def from_parts(cls, symm=None, skew=None):
@@ -20,7 +20,7 @@ class MandelSystem(BaseSystem):
         Parameters
         ----------
         symm: array (n, 6)
-          symmetric part as 6-vector in Mandel orthonormal basis
+          symmetric part as 6-vector in Voigt orthonormal basis
         skew: array (n, 3)
           skew part
         """
@@ -63,21 +63,21 @@ class MandelSystem(BaseSystem):
         m = matrices
         return np.array([
             m[:, 0, 0], m[:, 1, 1], m[:, 2, 2],
-            cls._s2i * (m[:, 1, 2] + m[:, 2, 1]),
-            cls._s2i * (m[:, 0, 2] + m[:, 2, 0]),
-            cls._s2i * (m[:, 0, 1] + m[:, 1, 0]),
-            cls._s2i * (m[:, 2, 1] - m[:, 1, 2]),
-            cls._s2i * (m[:, 0, 2] - m[:, 2, 0]),
-            cls._s2i * (m[:, 1, 0] - m[:, 0, 1,]),
+            0.5 * (m[:, 1, 2] + m[:, 2, 1]),
+            0.5 * (m[:, 0, 2] + m[:, 2, 0]),
+            0.5 * (m[:, 0, 1] + m[:, 1, 0]),
+            0.5 * (m[:, 2, 1] - m[:, 1, 2]),
+            0.5 * (m[:, 0, 2] - m[:, 2, 0]),
+            0.5 * (m[:, 1, 0] - m[:, 0, 1,]),
         ]).T
 
     @classmethod
     def to_matrices(cls, components):
         cm = components
         m11, m22, m33 = cm[:, 0], cm[:, 1], cm[:, 2]
-        m12, m21 = cls._s2i * (cm[:, 5] - cm[:, 8]), cls._s2i * (cm[:, 8] + cm[:, 5])
-        m13, m31 = cls._s2i * (cm[:, 4] + cm[:, 7]), cls._s2i * (cm[:, 4] - cm[:, 7])
-        m23, m32 = cls._s2i * (cm[:, 3] - cm[:, 6]), cls._s2i * (cm[:, 6] + cm[:, 3])
+        m12, m21 = (cm[:, 5] - cm[:, 8]), (cm[:, 8] + cm[:, 5])
+        m13, m31 = (cm[:, 4] + cm[:, 7]), (cm[:, 4] - cm[:, 7])
+        m23, m32 = (cm[:, 3] - cm[:, 6]), (cm[:, 6] + cm[:, 3])
         m = np.array([[m11, m12, m13], [m21, m22, m23], [m31, m32, m33]])
         return m.transpose((2, 0, 1))
 

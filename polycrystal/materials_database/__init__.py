45d96276504e09ee86cf77754116fe50cd489a58
donald e. boyce <donald.e.boyce@gmail.com>
2025-09-23 16:08:32 -0400

    adds code for materials database; elastic materials only, so far

---
+++ polycrystal/materials_database/__init__.py
@@ -0,0 +1,68 @@
+"""Load YAML database of materials"""
+
+import yaml
+
+from .loaders import get_loader
+
+
+class MaterialsDataBase:
+    """Material Database
+
+    Parameters
+    ----------
+    db_file: str or Path
+        name of YAML database file
+    """
+
+    def __init__(self, db_file):
+        self.db_file = db_file
+        self.processes = dict()
+        self._load_db()
+
+    def _load_db(self):
+        with open(self.db_file, "r") as f:
+            db = yaml.safe_load_all(f)
+            for doc in db:
+                if "process" not in doc:
+                    continue
+                process, matls = doc['process'], doc['materials']
+                self.processes[process] = {m['name']: m for m in matls}
+
+
+    def list_processes(self):
+        """List material processes"""
+        return list(self.processes.keys())
+
+    def list_materials(self, process):
+        """List material names for given process
+
+        Parameters
+        ----------
+        process: str
+           name of material process
+        """
+        return list(self.processes[process].keys())
+
+    def get_material(self, process, name):
+
+        """Return material data
+
+        Parameters
+        ----------
+        process: str
+           name of material process
+        name: str
+           name of material to return
+
+        Returns
+        -------
+        Instance
+           instance of material class correpsonding to material process
+        """
+        Loader = get_loader(process)
+        try:
+            entry = self.processes[name]
+        except:
+            raise KeyError(f"material '{name}' not found in database")
+
+        return Loader(entry)
